412a07f83812476172727ef7dee2d4307afa5cf3
Rudranil Mallick <rudranilmallick38@gmail.com>
2026-03-15 19:56:43 +0530

    updating script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+// smooth scrolling
+
+document.querySelectorAll("a").forEach(link=>{
+
+link.addEventListener("click",function(e){
+
+if(this.hash!==""){
+
+e.preventDefault()
+
+const target=document.querySelector(this.hash)
+
+target.scrollIntoView({
+behavior:"smooth"
+})
+
+}
+
+})
+
+})
+
+const toggle = document.getElementById("menu-toggle")
+const nav = document.getElementById("nav-links")
+
+toggle.addEventListener("click", () => {
+nav.classList.toggle("active")
+})
